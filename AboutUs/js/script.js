ad1db96b7881ae8abfde85f80d02539ae476df7a
masolangeg <marina.solange.g@gmail.com>
2024-04-26 21:42:59 -0300

    fix(helpuos/testimonials/about/adoptions): changes css - code - refactor file

--- AboutUs/js/script.js
+++ AboutUs/js/script.js
@@ -1,25 +1,25 @@
 // Desplazamiento suave al hacer click en cada sección
 
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('header nav a[href^="#"]');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const links = document.querySelectorAll('header nav a[href^="#"]');
     
-    links.forEach(link => {
-      link.addEventListener('click', function(e) {
-        e.preventDefault();
+//     links.forEach(link => {
+//       link.addEventListener('click', function(e) {
+//         e.preventDefault();
         
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
+//         const targetId = this.getAttribute('href').substring(1);
+//         const targetElement = document.getElementById(targetId);
         
-        if (targetElement) {
-          const offsetTop = targetElement.offsetTop;
-          window.scrollTo({
-            top: offsetTop,
-            behavior: 'smooth'
-          });
-        }
-      });
-    });
-  });
+//         if (targetElement) {
+//           const offsetTop = targetElement.offsetTop;
+//           window.scrollTo({
+//             top: offsetTop,
+//             behavior: 'smooth'
+//           });
+//         }
+//       });
+//     });
+//   });
 
 // // Efecto huella mouse
 // document.addEventListener('DOMContentLoaded', function() {
